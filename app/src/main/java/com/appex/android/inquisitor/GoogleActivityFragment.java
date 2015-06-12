2521f962344f05c4c3ca149aa42e1271969d602d
Anuraag Baishya <baishya.anuraag1920@gmail.com>
2015-06-12 19:07:13 +0530

    Minor Changes

--- app/src/main/java/com/appex/android/inquisitor/GoogleActivityFragment.java
+++ app/src/main/java/com/appex/android/inquisitor/GoogleActivityFragment.java
@@ -1,27 +1,32 @@
 package com.appex.android.inquisitor;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class GoogleActivityFragment extends Fragment {
+    private ProgressDialog pDialog;
     private String mUrl="https://www.google.com";
     private WebView mWebView;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        pDialog = new ProgressDialog(getActivity());
+        pDialog.setCancelable(true);
+        pDialog.setMessage("Loading...");
+        pDialog.setTitle("Please Wait");
+        pDialog.setIndeterminate(true);
     }
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,23 +34,29 @@ public class GoogleActivityFragment extends Fragment {
         final ProgressBar progressBar=(ProgressBar)rootView.findViewById(R.id.progressbar);
         progressBar.setMax(100);
         mWebView = (WebView)rootView.findViewById(R.id.webView);
-        mWebView.setWebViewClient(new WebViewClient() {
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-        });
-        mWebView.setWebChromeClient(new WebChromeClient(){
-            public void onProgressChanged(WebView view,int progress){
-                if (progress==100){
-                    progressBar.setVisibility(View.INVISIBLE);
-                }
-                else{
-                    progressBar.setVisibility(View.VISIBLE);
-                    progressBar.setProgress(progress);
-                }
-            }
-        });
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        mWebView.setWebViewClient(new MyBrowser());
         mWebView.loadUrl(mUrl);
         return rootView;
+    }
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            pDialog.show();
+            view.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            pDialog.dismiss();
+            view.setVisibility(View.VISIBLE);
+        }
     }
 }
